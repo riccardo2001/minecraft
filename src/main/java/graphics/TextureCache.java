a6c66c51061b7c5a70a812bf181c46b3dadcbb7b
riccardo2001 <riklupo2001@gmail.com>
2025-03-06 14:23:48 +0100

    changing path + creating solid jar

--- src/main/java/graphics/TextureCache.java
+++ src/main/java/graphics/TextureCache.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TextureCache {
 
-    public static final String DEFAULT_TEXTURE = "resources/models/default/default_texture.png";
+    public static final String DEFAULT_TEXTURE = "textures/default_texture.png";
 
     private Map<String, Texture> textureMap;
 
--- src/main/java/scene/SceneRender.java
+++ src/main/java/scene/SceneRender.java
@@ -33,9 +33,9 @@ public class SceneRender {
 
     public SceneRender() {
         List<ShaderProgram.ShaderModuleData> shaderModuleDataList = new ArrayList<>();
-        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER));
+        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("shaders/scene.vert", GL_VERTEX_SHADER));
         shaderModuleDataList
-                .add(new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER));
+                .add(new ShaderProgram.ShaderModuleData("shaders/scene.frag", GL_FRAGMENT_SHADER));
         shaderProgram = new ShaderProgram(shaderModuleDataList);
         createUniforms();
     }
